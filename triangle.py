cec4c490083e6df843063f0933762b3ab4b25974
Max-Wendt <74782038+Max-Wendt@users.noreply.github.com>
2020-11-23 08:18:28 -0600

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,5 +1,6 @@
-a = 0.5
-b = float(input("whats the base of the triangle in units: "))
+#variabl,and a is constant
+a = 0.5 
+b = float(input("whats the base of the triangle in units: ")) #both lines 3 and 4 are any possible positive number
 h = float(input("whats the height of the triangle in units: "))
 print ("the area of your triangle in units sqaured is, ")
-print (a*b*h) 
+print (a*b*h) #equation for the area of triangles
