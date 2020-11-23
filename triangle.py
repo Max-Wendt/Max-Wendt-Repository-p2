12829b0b99486142e9c2df730831c08cd8316752
Max-Wendt <74782038+Max-Wendt@users.noreply.github.com>
2020-11-23 08:16:30 -0600

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,5 @@
+a = 0.5
+b = float(input("whats the base of the triangle in units: "))
+h = float(input("whats the height of the triangle in units: "))
+print ("the area of your triangle in units sqaured is, ")
+print (a*b*h) 
