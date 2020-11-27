01db098e31e3601415a5df02203018ef1b7426e9
max-wendt <mwendt23@bsmschool.org>
2020-11-27 14:59:59 -0600

    Euler4

--- Euler4.py
+++ Euler4.py
@@ -1,5 +1,15 @@
-x = 1
-palidromels = []
 
-str = x <= 998001
-rev_str = str[::-1]
+palidromes = []
+
+for x in range (600, 1000):
+    for y in range (500, 1000):
+        num = x * y
+        rev = str(num)
+        rev = rev[::-1]
+        rev = int(rev)
+        if rev == num:
+            palidromes.append (num)
+        y += 1
+
+    x += 1
+print (max(palidromes))
