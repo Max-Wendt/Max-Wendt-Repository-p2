daae5ae1042056ac2965e5b05695d9d3a7b62d78
mwendt23 <74782038+mwendt23@users.noreply.github.com>
2020-12-03 19:18:24 -0600

    Update Euler3.py

--- Euler3.py
+++ Euler3.py
@@ -4,15 +4,15 @@ n = 1
 def primseive(n):
     for i in range (2, n - 1):
         if n % i == 0:
-          return false
-        return true
+          return False
+        return True
         
 while n <= 600851475143:
     n += 1
     if 600851475143 % n == 0:
          if n % 2 != 0:
-             if primseive(n) == true:
-                 primlist.append(n)
+             if primseive(n) == True:
+                 primelist.append(n)
 
 print(max(primelist))
 
