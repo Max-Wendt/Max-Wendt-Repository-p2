81f8126b49c5eff7abffe29356dd917f074fa813
max-wendt <mwendt23@bsmschool.org>
2020-12-04 08:17:54 -0600

    Completed Euler3

--- Euler3.py
+++ Euler3.py
@@ -4,11 +4,7 @@ n = 1
 def primseive(n):
     for i in range (2, n - 1):
         if n % i == 0:
-<<<<<<< HEAD
             return False
-=======
-          return False
->>>>>>> daae5ae1042056ac2965e5b05695d9d3a7b62d78
         return True
         
 while n <= 775146:
@@ -18,4 +14,4 @@ while n <= 775146:
              if primseive(n) == True:
                  primelist.append(n)
 
-print(max(primelist))
+print (max(primelist))
